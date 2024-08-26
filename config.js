e7595f1486e4d1a92f33be26dae475b17df6d05c
Kappi7581 <Kappi7581@outlook.com>
2024-08-26 16:46:02 +0300

    Update config.js

--- config.js
+++ config.js
@@ -22,19 +22,19 @@
 
 
 module.exports = {
-  TOKEN: "",
+  TOKEN: "MTI3Mjk2OTMzNDc4Mzc0MjAxMw.GxkxVE.fYyf3jYzef0R1hRExfh_QCEGnqxrP_k3IihqKI",
   ownerID: ["1272964504657068197"], 
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
-  embedColor: "#1db954",
+  embedColor: "#FF9400FF",
   musicardTheme:"themes16", //Goes from themes1 to themes19
   activityName: "Kappi7581 • Music", // This is bot status Write Anything here 
   activityType: "WATCHING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/vjk7XAHaQf",
-  CheckmarkIcon: "https://cdn.discordapp.com/attachments/1230824451990622299/1230836684774576168/7762-verified-blue.gif",
+  CheckmarkIcon: "https://cdn.discordapp.com/emojis/1277622865281417278.gif?v=1&size=48&quality=lossless",
   MusicIcon:"https://media.discordapp.net/attachments/1230824451990622299/1236664581364125787/music-play.gif",
   embedTimeout: 5,  // Timeout before the button interaction embeds are deleted ( Default - 5 seconds)
-  errorLog: "", 
+  errorLog: "1277624744786526228", 
 
    // Lavalink Server Details
 
